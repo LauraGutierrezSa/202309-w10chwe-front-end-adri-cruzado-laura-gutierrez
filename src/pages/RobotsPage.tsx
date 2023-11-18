ba055c58c8c6b43de6e89dca198de7baa2e06a71
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-18 19:02:33 +0100

    Add RobotsPage page

---
+++ src/pages/RobotsPage.tsx
@@ -0,0 +1,11 @@
+import StyledRobotsPage from "./RobotsPageStyled";
+
+const RobotsPage = () => {
+  return (
+    <StyledRobotsPage>
+      <h1 className="robots-page-title">Explore robots:</h1>
+    </StyledRobotsPage>
+  );
+};
+
+export default RobotsPage;
