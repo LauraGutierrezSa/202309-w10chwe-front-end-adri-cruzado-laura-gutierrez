183af3acd5297057a89dec828658287435a1b936
Adri Cruzado <adricruzado96@gmail.com>
2023-11-19 21:14:28 +0100

    Update handlers imports

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,10 +3,8 @@ import { robotsMock } from "./robots/robotsMock";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-const handlers = [
+export const handlers = [
   http.get(`${apiUrl}/robots`, () => {
     return HttpResponse.json(robotsMock);
   }),
 ];
-
-export default handlers;
