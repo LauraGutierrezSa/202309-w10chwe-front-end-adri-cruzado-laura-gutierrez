65e63f0f26bf52acb2d74c516fff0184f8c67944
Adri Cruzado <adricruzado96@gmail.com>
2023-11-19 14:04:44 +0100

    Add useRobotsApi custom hook

---
+++ src/store/features/robots/types.ts
@@ -0,0 +1,14 @@
+export interface RobotStructureWithoutId {
+  name: string;
+  imageUrl: string;
+  speed: number;
+  endurance: number;
+}
+
+export interface RobotStructure extends RobotStructureWithoutId {
+  _id: string;
+}
+
+export interface RobotsStateStructure {
+  robots: RobotStructure[];
+}
